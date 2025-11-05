1adbfdd47c24b22b12c5aed088a5dd807bb552c6
EpikPotato <duccute3@gmail.com>
2025-11-05 07:03:09 +0700

    env file

--- database/connect.go
+++ database/connect.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"myproject/models"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,20 +14,34 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=admin dbname=urlshortener port=5432 sslmode=disable"
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbname, port, sslmode,
+	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalf(" Failed to connect to database: %v", err)
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
-	err = db.AutoMigrate(
-		&models.ShortURL{},
-	)
+	err = db.AutoMigrate(&models.ShortURL{})
 	if err != nil {
-		log.Fatalf(" Failed to migrate database: %v", err)
+		log.Fatalf("Failed to migrate database: %v", err)
 	}
 
 	DB = db
-	fmt.Println(" Connected to PostgreSQL and auto-migrated successfully.")
+	fmt.Println("Connected to PostgreSQL and auto-migrated successfully.")
 }
